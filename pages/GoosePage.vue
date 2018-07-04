5c862ef2d280f9d63d17be2b900d0df86d566a82
Gustavo Adolfo Arellano Sandoval <garellano@mac.local>
2018-07-04 09:22:10 -0500

    First commit

---
+++ pages/GoosePage.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+  	<h1>Hello Goose</h1>
+		<button type="button" class="btn btn-primary btn-lg">Primary</button>
+		<button type="button" class="btn btn-secondary">Secondary</button>
+		<button type="button" class="btn btn-success btn-lg">Success</button>
+		<button type="button" class="btn btn-danger">Danger</button>
+		<button type="button" class="btn btn-warning btn-sm">Warning</button>
+		<button type="button" class="btn btn-info">Info</button>
+		<button type="button" class="btn btn-light btn-sm">Light</button>
+		<button type="button" class="btn btn-dark">Dark</button>
+		<button type="button" class="btn btn-link">Link</button>  
+<hr/>
+
+<div class="alert alert-primary" role="alert">
+  This is a primary alert—check it out!
+</div>
+<div class="alert alert-secondary" role="alert">
+  This is a secondary alert—check it out!
+</div>
+<div class="alert alert-success" role="alert">
+  This is a success alert—check it out!
+</div>
+<div class="alert alert-danger" role="alert">
+  This is a danger alert—check it out!
+</div>
+<div class="alert alert-warning" role="alert">
+  This is a warning alert—check it out!
+</div>
+<div class="alert alert-info" role="alert">
+  This is a info alert—check it out!
+</div>
+
+
+<div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div>
+        <h1><label id="rate">0</label></h1>
+        <h1><label id="tipo">ok</label></h1>
+</div>
+
+
+  </div>
+</template>
+
+
+        <script>
+            var wsocket;
+            function connect() {
+                wsocket = new WebSocket("ws://localhost:8080/WebSocket/websocketendpoint");
+                wsocket.onmessage = onMessage;
+            }
+            function onMessage(evt) {
+                var page = JSON.parse(evt.data);
+                document.getElementById("rate").innerHTML = page.a;
+                document.getElementById("tipo").innerHTML = page.b;
+            }
+            window.addEventListener("load", connect, false);
+        </script>
